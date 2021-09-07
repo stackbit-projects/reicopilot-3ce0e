9587cacfa64d35574c219e1c34c8850dc4c1fbd3
Trey White <treywhite21@gmail.com>
2021-09-07 00:02:05 -0400

    Update Tailwind config to purge necessary files:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
+    // Add more here
+  ],
   darkMode: 'media', // false or 'media' or 'class'
   theme: {
     extend: {},
