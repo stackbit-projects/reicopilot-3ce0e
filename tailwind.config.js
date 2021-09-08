15b2d80807b6b8333dcb45038857f38c0a9d200c
Trey White <treywhite21@gmail.com>
2021-09-07 18:17:49 -0700

    Fix tailwind purge paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: [
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/layouts/**/*.{js,ts,jsx,tsx}',
+    ],
     darkMode: 'media', // false or 'media' or 'class'
     theme: {
         colors: {
