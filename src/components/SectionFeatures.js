8990813f8b06232a694513646e97b74fcf475a8a
Trey White <treywhite21@gmail.com>
2021-09-08 09:56:55 -0700

    Create new features section template

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -1,55 +1,274 @@
 import React from 'react';
 import _ from 'lodash';
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, MailIcon, ScaleIcon } from '@heroicons/react/outline';
 
 import { htmlToReact, withPrefix, markdownify } from '../utils';
 import CtaButtons from './CtaButtons';
 
+const transferFeatures = [
+  {
+    id: 1,
+    name: 'Competitive exchange rates',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeAltIcon,
+  },
+  {
+    id: 2,
+    name: 'No hidden fees',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ScaleIcon,
+  },
+  {
+    id: 3,
+    name: 'Transfers are instant',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: LightningBoltIcon,
+  },
+]
+const communicationFeatures = [
+  {
+    id: 1,
+    name: 'Mobile notifications',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: AnnotationIcon,
+  },
+  {
+    id: 2,
+    name: 'Reminder emails',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: MailIcon,
+  },
+]
+
 export default class SectionFeatures extends React.Component {
     renderFeature(feature, index) {
-        const image = _.get(feature, 'image');
-        const imageAlt = _.get(feature, 'image_alt');
-        const title = _.get(feature, 'title');
-        const content = _.get(feature, 'content');
-        const actions = _.get(feature, 'actions');
+      const image = _.get(feature, 'image');
+      const imageAlt = _.get(feature, 'image_alt');
+      const title = _.get(feature, 'title');
+      const content = _.get(feature, 'content');
+      const actions = _.get(feature, 'actions');
 
-        return (
-            <div key={index} className="block-item">
-                <div className="grid">
-                    {image && (
-                        <div className="cell block-preview">
-                            <img src={withPrefix(image)} alt={imageAlt} />
-                        </div>
-                    )}
-                    <div className="cell block-content">
-                        <h3 className="block-title underline">{title}</h3>
-                        <div className="block-copy">{markdownify(content)}</div>
-                        {actions && (
-                            <div className="block-buttons">
-                                <CtaButtons actions={actions} />
-                            </div>
-                        )}
-                    </div>
+      return (
+        <div key={index} className="block-item">
+          <div className="grid">
+            {image && (
+              <div className="cell block-preview">
+                <img src={withPrefix(image)} alt={imageAlt} />
+              </div>
+            )}
+            <div className="cell block-content">
+              <h3 className="block-title underline">{title}</h3>
+              <div className="block-copy">{markdownify(content)}</div>
+              {actions && (
+                <div className="block-buttons">
+                  <CtaButtons actions={actions} />
                 </div>
+              )}
             </div>
-        );
+          </div>
+        </div>
+      );
     }
 
     render() {
-        const section = _.get(this.props, 'section');
-        const sectionId = _.get(section, 'section_id');
-        const background = _.get(section, 'background');
-        const title = _.get(section, 'title');
-        const subtitle = _.get(section, 'subtitle');
-        const features = _.get(section, 'features');
+      const section = _.get(this.props, 'section');
+      const sectionId = _.get(section, 'section_id');
+      const background = _.get(section, 'background');
+      const title = _.get(section, 'title');
+      const subtitle = _.get(section, 'subtitle');
+      const features = _.get(section, 'features');
 
-        return (
-            <section id={sectionId} className={`block features-block bg-${background} outer`}>
-                <div className="block-header inner-small">
-                    {title && <h2 className="block-title">{title}</h2>}
-                    {subtitle && <p className="block-subtitle">{htmlToReact(subtitle)}</p>}
+      return (
+        <div className="py-16 bg-white dark:bg-gray-900 overflow-hidden lg:py-24">
+          <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+            <svg
+              className="hidden lg:block absolute left-full transform -translate-x-1/2 -translate-y-1/4"
+              width={404}
+              height={784}
+              fill="none"
+              viewBox="0 0 404 784"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect x={0} y={0} width={4} height={4} className="text-gray-200 dark:text-gray-700" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+            </svg>
+
+            <div className="relative">
+              <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-white dark:text-gray-300 sm:text-4xl">
+                AI-empowered investing
+              </h2>
+              <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500 dark:text-gray-300">
+                Stop trying to do it all by yourself. It's
+                {` ${new Date().getFullYear()}`}
+                , invest like it. Leverage the power of artificial intelligence to monitor and guide
+                your investments.
+              </p>
+            </div>
+
+            <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+              <div className="relative">
+                <h3 className="text-2xl font-extrabold text-gray-900 dark:text-white tracking-tight sm:text-3xl">
+                  Transfer funds world-wide
+                </h3>
+                <p className="mt-3 text-lg text-gray-500 dark:text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae, porro maiores
+                  officia assumenda aliquam laborum ab aliquid veritatis impedit odit adipisci optio iste blanditiis facere.
+                  Totam, velit.
+                </p>
+
+                <dl className="mt-10 space-y-10">
+                {transferFeatures.map((item) => (
+                  <div key={item.id} className="relative">
+                    <dt>
+                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <item.icon className="h-6 w-6" aria-hidden="true" />
+                      </div>
+                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900 dark:text-white">{item.name}</p>
+                    </dt>
+                    <dd className="mt-2 ml-16 text-base text-gray-500 dark:text-gray-300">{item.description}</dd>
+                  </div>
+                ))}
+                </dl>
+              </div>
+
+              <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+                <svg
+                  className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                  width={784}
+                  height={404}
+                  fill="none"
+                  viewBox="0 0 784 404"
+                >
+                  <defs>
+                    <pattern
+                      id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect x={0} y={0} width={4} height={4} className="text-gray-200 dark:text-gray-700" fill="currentColor" />
+                    </pattern>
+                  </defs>
+                  <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
+                </svg>
+                <img
+                  className="relative mx-auto"
+                  width={490}
+                  src="https://tailwindui.com/img/features/feature-example-1.png"
+                  alt=""
+                />
+              </div>
+            </div>
+
+              <svg
+                className="hidden lg:block absolute right-full transform translate-x-1/2 translate-y-12"
+                width={404}
+                height={784}
+                fill="none"
+                viewBox="0 0 404 784"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200 dark:text-gray-700" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+              </svg>
+
+              <div className="relative mt-12 sm:mt-16 lg:mt-24">
+                <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+                  <div className="lg:col-start-2">
+                    <h3 className="text-2xl font-extrabold text-gray-900 dark:text-white tracking-tight sm:text-3xl">Always in the loop</h3>
+                    <p className="mt-3 text-lg text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex obcaecati natus eligendi delectus,
+                      cum deleniti sunt in labore nihil quod quibusdam expedita nemo.
+                    </p>
+
+                    <dl className="mt-10 space-y-10">
+                    {communicationFeatures.map((item) => (
+                      <div key={item.id} className="relative">
+                        <dt>
+                          <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                            <item.icon className="h-6 w-6" aria-hidden="true" />
+                          </div>
+                          <p className="ml-16 text-lg leading-6 font-medium text-gray-900 dark:text-white">{item.name}</p>
+                        </dt>
+                        <dd className="mt-2 ml-16 text-base text-gray-500 dark:text-gray-300">{item.description}</dd>
+                      </div>
+                    ))}
+                  </dl>
                 </div>
-                {features && <div className="inner">{_.map(features, (feature, index) => this.renderFeature(feature, index))}</div>}
-            </section>
-        );
+
+                <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+                  <svg
+                    className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                    width={784}
+                    height={404}
+                    fill="none"
+                    viewBox="0 0 784 404"
+                    aria-hidden="true"
+                  >
+                    <defs>
+                      <pattern
+                        id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                        x={0}
+                        y={0}
+                        width={20}
+                        height={20}
+                        patternUnits="userSpaceOnUse"
+                      >
+                        <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                      </pattern>
+                    </defs>
+                    <rect width={784} height={404} fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
+                  </svg>
+                  <img
+                    className="relative mx-auto"
+                    width={490}
+                    src="https://tailwindui.com/img/features/feature-example-2.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+
+      return (
+        <section id={sectionId} className={`block features-block bg-${background} outer`}>
+          <div className="block-header inner-small">
+            {title && <h2 className="block-title">{title}</h2>}
+            {subtitle && <p className="block-subtitle">{htmlToReact(subtitle)}</p>}
+          </div>
+          {features && <div className="inner">{_.map(features, (feature, index) => this.renderFeature(feature, index))}</div>}
+        </section>
+      );
     }
 }
