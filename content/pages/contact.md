6ef1a3a39804da592855faa804fc218d8597d7da
Trey White <treywhite21@gmail.com>
2021-09-10 16:54:36 -0700

    Finish basic Contact form

--- content/pages/contact.md
+++ content/pages/contact.md
@@ -3,39 +3,62 @@ title: Contact
 sections:
   - section_id: contact
     type: section_contact
-    title: Contact
-    content: >-
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a metus
-      quis lorem malesuada luctus. Cras lacinia, eros at dapibus molestie, risus
-      tortor pretium ligula.
+    title: Contact Us
+    subtitle: >-
+
     form_id: contactForm
     form_fields:
       - input_type: text
-        name: name
-        label: Name
+        name: first-name
+        label: First name
+        auto_complete: given-name
         is_required: true
+        col_span: 1
+      - input_type: text
+        name: last-name
+        label: Last name
+        auto_complete: family-name
+        is_required: true
+        col_span: 1
       - input_type: email
         name: email
         label: Email
+        auto_complete: email
         is_required: true
-      - input_type: select
-        name: subject
-        label: Subject
-        default_value: Please select
-        options:
-          - Error on the site
-          - Sponsorship
-          - Other
+        col_span: 2
+      - input_type: text
+        name: phone-number
+        label: Phone
+        auto_complete: tel
+        is_required: false
+        default_value: +1 (555) 987-6543
+        col_span: 2
       - input_type: textarea
-        name: message
-        label: Message
-      - input_type: checkbox
-        name: consent
-        label: >-
-          I understand that this form is storing my submitted information so I
-          can be contacted.
+        name: how-can-we-help
+        label: How can we help you?
         is_required: true
-    submit_label: Send Message
+        col_span: 2
+      - input_type: radio
+        name: number-of-investments
+        label: Number of investments
+        is_required: true
+        options:
+          - id: investments-under-5
+            label: Less than 5
+          - id: investments-5-to-25
+            label: 5 to 25
+          - id: investments-25-to-50
+            label: 25 to 50
+          - id: investments-above-50
+            label: 50+
+        col_span: 2
+      - input_type: text
+        name: how-did-you-hear-about-us
+        label: How did you hear about us?
+        is_required: false
+        col_span: 2
+    submit_label: Let's talk
+    hide_labels: true
 seo:
   title: Contact
   description: This is the contact page
--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,60 +1,142 @@
 import React from 'react';
-import _ from 'lodash';
+import get from 'lodash/get';
+import map from 'lodash/map';
 
 import { classNames } from '../utils';
 
-export default class FormField extends React.Component {
-    render() {
-        const field = _.get(this.props, 'field');
-        const hideLabels = _.get(this.props, 'hideLabels');
-        const inputType = _.get(field, 'input_type');
-        const label = _.get(field, 'label');
-        const name = _.get(field, 'name');
-        const defaultValue = _.get(field, 'default_value');
-        const options = _.get(field, 'options');
-        const required = _.get(field, 'is_required');
-        const attr = {};
-        const nameLabel = `${name}-label`;
-        if (label) {
-            attr['aria-labelledby'] = nameLabel;
-        }
-        if (required) {
-            attr.required = true;
-        }
+const FormField = (props) => {
+  const field = get(props, 'field');
+  const hideLabels = get(props, 'hideLabels');
+  const inputType = get(field, 'input_type');
+  const label = get(field, 'label');
+  const name = get(field, 'name');
+  const autoComplete = get(field, 'autocomplete_value');
+  const defaultValue = get(field, 'default_value');
+  const options = get(field, 'options');
+  const required = get(field, 'is_required');
+  const colSpan = get(field, 'col_span');
+  const attr = {};
+  const nameLabel = `${name}-label`;
 
-        return (
-            <React.Fragment>
-                {inputType !== 'checkbox' && label && (
-                    <label id={nameLabel} htmlFor={name} className={classNames({ 'screen-reader-text': hideLabels })}>
-                        {label}
-                    </label>
-                )}
-                {inputType === 'checkbox' ? (
-                    <div className="form-checkbox">
-                        <input id={name} type="checkbox" name={name} {...attr} />
-                        {label && (
-                            <label htmlFor={name} id={nameLabel}>
-                                {label}
-                            </label>
-                        )}
-                    </div>
-                ) : inputType === 'select' ? (
-                    <div className="form-select">
-                        <select id={name} name={name} {...attr}>
-                            {defaultValue && <option value="">{defaultValue}</option>}
-                            {_.map(options, (option, optionIdx) => (
-                                <option key={optionIdx} value={option}>
-                                    {option}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                ) : inputType === 'textarea' ? (
-                    <textarea id={name} name={name} rows="5" {...(defaultValue ? { placeholder: defaultValue } : null)} {...attr} />
-                ) : (
-                    <input id={name} type={inputType} name={name} {...(defaultValue ? { placeholder: defaultValue } : null)} {...attr} />
-                )}
-            </React.Fragment>
-        );
-    }
+  if (label) {
+    attr['aria-labelledby'] = nameLabel;
+  }
+  if (required) {
+    attr.required = true;
+  }
+
+  return (
+    <React.Fragment>
+      {inputType !== 'checkbox' && label && (
+        <label id={nameLabel} htmlFor={name} className={classNames({ 'sr-only': hideLabels })}>
+          {label}
+        </label>
+      )}
+      {inputType === 'checkbox' ? (
+        <div className="form-checkbox">
+          <input id={name} type="checkbox" name={name} {...attr} />
+          {label && (
+            <label htmlFor={name} id={nameLabel}>
+              {label}
+            </label>
+          )}
+        </div>
+      ) : inputType === 'radio' ? (
+        <fieldset className={classNames({
+          'sm:col-span-2': colSpan && colSpan === 2, 
+        })}>
+          <legend className="block text-sm font-medium text-gray-700 dark:text-gray-200">
+            {label}
+          </legend>
+          <div className="mt-4 grid grid-cols-1 gap-y-4">
+            {map(options, (option) => {
+              const optionId = get(option, 'id');
+              const optionLabel = get(option, 'label');
+
+              return (
+                <div key={optionId} className="flex items-center">
+                  <input
+                    id={optionId}
+                    name={name}
+                    defaultValue={optionId}
+                    type="radio"
+                    className="focus:ring-primary-500 h-4 w-4 text-primary-500
+                      border-gray-300 dark:border-gray-600"
+                  />
+                  <label htmlFor={optionId} className="ml-3">
+                    <span className="block text-sm text-gray-700 dark:text-gray-200">{optionLabel}</span>
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+        </fieldset>
+      ) : inputType === 'select' ? (
+          <div className="form-select">
+            <select id={name} name={name} {...attr}>
+              {defaultValue && <option value="">{defaultValue}</option>}
+              {map(options, (option, optionIdx) => (
+                <option key={optionIdx} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+      ) : inputType === 'textarea' ? (
+          <div className={classNames({
+            'sm:col-span-2': colSpan && colSpan === 2, 
+          })}>
+            <div className="flex justify-between">
+              <label
+                htmlFor={name}
+                className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+              >
+                {label}
+              </label>
+              <span id={`${name}-description`} className="text-sm text-gray-500 dark:text-gray-300">
+                Max. 500 characters
+              </span>
+            </div>
+            <div className="mt-1">
+              <textarea
+                id={name}
+                name={name}
+                aria-describedby={`${name}-description`}
+                rows={4}
+                className="block w-full shadow-sm sm:text-sm focus:ring-primary-500 dark:bg-gray-900 dark:text-white
+                  focus:border-primary-500 border border-gray-300 dark:border-gray-600 rounded-md"
+                maxLength="500"
+                {...(defaultValue ? { placeholder: defaultValue } : null)}
+                {...attr}
+              />
+            </div>
+          </div>
+      ) : (
+        <div className={classNames({
+          'sm:col-span-2': colSpan && colSpan === 2, 
+        })}>
+          <label
+            htmlFor={name}
+            className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+          >
+            {label}
+          </label>
+          <div className="mt-1">
+            <input
+              type={inputType}
+              name={name}
+              id={name}
+              autoComplete={autoComplete}
+              className="py-3 px-4 block w-full shadow-sm focus:ring-primary-500 focus:border-primary-500
+                border-gray-300 dark:border-gray-600 dark:bg-gray-900 dark:text-white rounded-md"
+              {...(defaultValue ? { placeholder: defaultValue } : null)}
+              {...attr}
+            />
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
+
+export default FormField;
