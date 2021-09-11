4bdfd486e168f407065a8a3468e1be32a84a9343
Trey White <treywhite21@gmail.com>
2021-09-10 23:36:11 -0700

    Populate our story content

--- content/pages/our-story.md
+++ content/pages/our-story.md
@@ -1,10 +1,9 @@
 ---
 pretitle: REI Copilot
-title: Our Story
+title: >-
+  Our Story: The "Aha" Moment
 subtitle: >-
-  The style guide provides you with a blueprint of default post and page styles.
-  The style guide is also a great reference for suggested typographic treatment
-  and styles for your content.
+  Several years ago, I finally realized that almost everything I have been taught about finances and being successful was wrong! A very disheartening discovery to say the least, especially for someone who graduated from dental school way back in 1984. Like many of you, I was taught from an early age to work hard, make good grades, and get into a respectable college and a good job with benefits would be my reward. Those habits continued through dental school and into my orthodontic residency. Through hard work and sheer determination, I was able to build a successful orthodontic practice which has provided for a very comfortable life for me and my family. I was also taught to live on less than I make, save my money and get out of debt which I faithfully did. These disciplines can create a life of security, but they can’t provide freedom, and in the end, that’s what most of us want . . . freedom! Do you have enough money to retire? How much is enough? Do you have a plan? More importantly, do you have a date? These are the questions most of us start asking toward the end of our careers. Of course, the time to ask these questions is at the beginning!
 sections:
   - id: our-story
     type: our_story
--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -1,63 +1,217 @@
 import React from 'react';
+import { withPrefix } from '../utils';
 
 const OurStory = () => (
-  <div className="mt-6 prose prose-primary prose-lg text-gray-500 dark:text-gray-300 mx-auto">
-    <p>
-      Faucibus commodo massa rhoncus, volutpat. <strong className="!text-primary-500">Dignissim</strong> sed
-      <strong className="!text-primary-500">eget risus enim</strong>.
-      Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra
-      tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
-      <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
-    </p>
-    <ul role="list">
-      <li>Quis elit egestas venenatis mattis dignissim.</li>
-      <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-      <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-    </ul>
-    <p>
-      Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam
-      porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc
-      purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.
-    </p>
-    <h2>From beginner to expert in 30 days</h2>
-    <p>
-      Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
-      Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus
-      mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
-      ipsum eu a sed convallis diam.
-    </p>
-    <blockquote>
-      <p>
-        Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
-        pellentesque. Blandit amet, sed aenean erat arcu morbi.
+  <React.Fragment>
+    <div className="mt-6 prose prose-primary prose-lg text-gray-500 dark:text-gray-300 mx-auto">
+      <span className="sr-only">Make money while you sleep</span>
+      <h2 className="dark:!text-white">Make money while you sleep</h2>
+      <p className="dark:text-gray-300">
+      Unfortunately, most of us rely on financial advisors who have had surprisingly little investment 
+      education and simply “plug” your financial details into a “proprietary” formula and out comes your 
+      customized plan, which looks eerily similar to most of our colleagues plans except for the numbers. 
+      Sadly, regardless of the performance of your plan, these financial advisors collect fees and 
+      commissions every month. Ask me how I know! Years ago, I determined that I could do just as well in 
+      the stock market as the advisors to whom I had paid all of those fees and commissions for so many 
+      years. To be honest, I was even a little better, especially these last few years. Then I discovered 
+      the concept of passive income, where your money works for you. I immediately called my “broker” and 
+      asked about the amount of passive income that my stock portfolio was creating. Now mind you, I was 
+      only investing in blue-chip stocks that paid a quarterly dividend. As I mentioned, I had done quite 
+      well recently so imagine my shock when the amount of passive income that my portfolio was generating 
+      in the last twelve months was enough to cover our expenses and fund our lifestyle for about ONE month! 
+      OMG! The financial wake up call jolted me into action and I immediately purposed to find a better way.
       </p>
-    </blockquote>
-    <p>
-      Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
-      sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
-    </p>
-    <figure>
-      <img
-        className="w-full rounded-lg"
-        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-        alt=""
-        width={1310}
-        height={873}
-      />
-      <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
-    </figure>
-    <h2>Everything you need to get up and running</h2>
-    <p>
-      Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus
-      volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus
-      non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc,
-      congue erat ac. Cras fermentum convallis quam.
-    </p>
-    <p>
-      Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
-      sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
-    </p>
-  </div>
+      <figure>
+        <img
+          className="w-full rounded-lg"
+          src={withPrefix('images/about/about-buffet.jpg')}
+          alt="Warren Buffet quote"
+          width={1310}
+          height={873}
+        />
+        <figcaption>Warren Buffet speaks on investing for freedom</figcaption>
+      </figure>
+
+      <h2 className="dark:!text-white">A change in mindset</h2>
+      <p className="dark:text-gray-300">
+      Ever since I was a kid, I’ve hated reading. But, as an adult, I have discovered that most of the 
+      successful people that I have met are voracious readers. So, like they say, “when in Rome!” One of 
+      the first financial books that I read was “Rich Dad Poor Dad” by Robert Kiyosaki. This is a must 
+      read for anyone seeking to change the way they think about finances. The biggest takeaway for me 
+      was that the Rich don’t work for money, they acquire assets that make money for them. This is known 
+      as passive income and is the key to building wealth.
+      </p>
+      <p className="dark:text-gray-300">
+      For most of us, we must be in our practice seeing patients in order to generate income. This is 
+      what Kiyosaki calls trading time for dollars. Few of us can be away from the office for more than 
+      two weeks before the bottom line starts to suffer. This is the medical version of “Golden Handcuffs” 
+      as we’re paid well, but we must be in the office to make money. Some of us have partners or associates 
+      who can cover for emergencies and lessen the financial deficit somewhat. However, the bottom line is 
+      that we must work to make money. As the economy tightens and insurance companies reimburse less and 
+      less each year, we’re trading more time for fewer dollars. It’s easy to feel like the proverbial 
+      hamster on the wheel. It can seem as if there is no hope!
+      </p>
+      <figure>
+        <img
+          className="w-full rounded-lg"
+          src={withPrefix('images/about/about-rich-dad.jpg')}
+          alt="Rich Dad Poor Dad"
+          width={1310}
+          height={873}
+        />
+        <figcaption>Rich Dad Poor Dad - a paradigm shift in how we think about finances</figcaption>
+      </figure>
+
+      <h2 className="dark:!text-white">Income vs. Wealth</h2>
+      <p className="dark:text-gray-300">
+      Another concept that Kiyosaki introduced is the Cashflow Quadrant which qualifies the four ways 
+      that people make money. In the “Employee” quadrant, you have a job. Highly compensated people can 
+      be included in this category which usually also includes good benefits. In the “Self-Employment” 
+      quadrant, you actually own a job. Both quadrants on the left side of the diagram offer security 
+      but an employee must work to earn income, trading time for dollars. The third quadrant is the 
+      Business Owner (greater than 500 employees by definition). In this quadrant, you own a system and 
+      other people make money for you. The Investor is the final quadrant where your money works for you. 
+      In both of the right-side quadrants, wealth is created by passive income. Since it is not necessary 
+      for you to be actively working in the business, you have the freedom and the income to go where ever 
+      you want and do what ever you want to do, now that’s true freedom!
+      </p>
+      <p className="dark:text-gray-300">
+      So, the key to creating passive income and wealth which leads to financial freedom is to acquire 
+      assets that can generate income. The asset that I’ve found to be the most effective in so many ways 
+      is real estate.
+      </p>
+      <figure>
+        <img
+          className="w-full rounded-lg"
+          src={withPrefix('images/about/about-cash-flow-quadrant.png')}
+          alt="Rich Dad Poor Dad"
+          width={1310}
+          height={873}
+        />
+        <figcaption>Robert Kiyosaki's Cash Flow Quadrant</figcaption>
+      </figure>
+
+      <h2 className="dark:!text-white">The problem with investing</h2>
+      <p className="dark:text-gray-300">
+      I poured most of my energy into building my practice and raising two boys with my wife. My efforts 
+      paid off and we were able to pay off all of our debts and increase our savings.
+      </p>
+      <p className="dark:text-gray-300">
+      Since I owned a good job and my wife and I were disciplined enough to live on less than what we 
+      earned, we were able to save a substantial amount of money. After understanding the importance and 
+      the urgency of building wealth, I immediately joined a real estate investing group. Immediately 
+      after I joined the group, I bought several single-family rental houses and made several hard money 
+      loans to a few trusted real estate developers. And for a while I was feeling pretty good about our 
+      progress toward wealth building. That is, until I started getting phone calls asking for more money. 
+      All of the rental houses that we purchased required significant renovation before they would be ready 
+      to rent. I got a phone call from one developer saying he needed another $18,000 more for the rehab 
+      and another developer would call saying that he needed the next $15,000 for yet another rehab. To be 
+      honest, I had no way of knowing if that was the right amount or not. And because I trusted them, I 
+      just wrote the check. But in reality, they could have ended up charging me more than we had agreed 
+      to and I wouldn’t have known it until well after the deal was completed and my accountant had looked 
+      over the numbers at tax time.
+      </p>
+      <p className="dark:text-gray-300">
+      As a matter of fact, with one of my houses I found out after the fact it was $8,000 over budget 
+      because the developer had actually put new windows in the house without telling me. Now while I 
+      probably would have approved that upgrade, I didn’t know about it until long after the house was 
+      rented. I know that I’m making excuses, but running a busy orthodontic practice and keeping up with 
+      all of my real estate investments was becoming overwhelming. It definitely would have been better if 
+      I had been involved with a conversation about the decision to upgrade the windows. If I had been able 
+      to easily find out what the budget was and how much money I’d already given them, I would have 
+      immediately known that we were “over budget” which would have prompted questions about the reason 
+      why the developer was still asking for more money and I would’ve have known about the windows. But 
+      you know, I was busy and I didn’t have time to stop what I was doing to find out if the amount the 
+      developer was asking for was in budget. So, I just wrote the check, not a good way to run a real 
+      estate investing business to say the least! At the same time, I got an email informing me that a 
+      particular lending deal was closing on Monday the fifth and wanted me to verify the payoff amount. 
+      I would have loved to have answered that right away. But it wasn’t that simple. As a matter of fact, 
+      it was rather complicated.
+      </p>
+      <p className="dark:text-gray-300">
+      First, I had to go find the original documents, including the promissory note that outline the 
+      details of the hard money loan, to find out what the interest rate was and the amount of the loan 
+      was and if there were any points included or any shared equity from the sale of the investment 
+      property. Then, I had to calculate my monthly and daily earnings and the length of the loan period 
+      to determine the amount of money that was owed to me. After that, I had to go back and find the 
+      account that coincided with this particular loan and then total up all the incoming monthly payments 
+      that I had received. Once that was done, I had to subtract that amount from the anticipated total 
+      income from the loan to get a payoff amount! Whew!
+      </p>
+      <p className="dark:text-gray-300">
+      Well, you can imagine in the middle of a busy day that the process took several hours for me to get 
+      the information together. And you can imagine how frustrated I was feeling. This didn’t feel like 
+      passive income at all! So I started asking my real estate investing friends how they were able to 
+      keep track of all of their real estate investments? Shockingly, a couple of my friends said me that 
+      they weren’t really keeping up with their portfolio. Then other friends reported using spreadsheets. 
+      But one was using a spreadsheet that his brother designed and another was using a spreadsheet that an 
+      investor friend had given him. Of course, none of them were the same. And so I became very frustrated 
+      and disillusioned with the chaotic way that people were tracking their investments. Interestingly, most 
+      investors that I encounter at various real estate groups can’t tell you what the return on their 
+      investment is for any of their properties!
+      </p>
+
+      <h2 className="dark:!text-white">The search for a solution</h2>
+      <p className="dark:text-gray-300">
+      So, I started looking on-line for software solutions to address my problem in managing my growing 
+      real estate portfolio. I was amazed to find that there really isn’t much out there other than 
+      someone’s favorite spread sheet. I then asked my son, who is a skilled computer programmer for Microsoft, if he 
+      would make a spread sheet for my investments. He asked me about the things that I wanted to 
+      accomplish and when I answered him, he informed me that I need much more than a spreadsheet could 
+      provide. He told me that in order to provide all of the functionality that I wanted in managing my 
+      portfolio, I would need a web-based application. Initially, when we started to developing the app, 
+      we thought it was going to be easy, but the more questions I answered and the more information that 
+      I gave him, we quickly realized this was going to be a much more complex and time intensive solution 
+      than we had anticipated. So, we decided right then and there that we would not only help solve my 
+      problems with real estate investing, but we would be on a mission to help other frustrated and 
+      confused real estate investors as well. Our goal would be to put the “Passive” back in passive 
+      income because it has become clear to me that most real estate investors have no idea what their 
+      numbers are! And that became the impetus for developing our interactive, real-time real estate 
+      portfolio management software called REI Copilot. When we first started, I mapped out all of the 
+      features and functionality necessary to accurately and passively manage the complexities of a 
+      real estate investment portfolio. Through our collaboration, we have been able to create an 
+      amazing software tool! As so many real estate investors have commented, “there’s nothing out 
+      there like REI Copilot! This comprehensive real estate portfolio management software not only 
+      stores all of the necessary documents and contacts in one place, but an investor is able to track 
+      the performance of their entire portfolio as well as asset classes and even individual assets on 
+      their smartphone or desktop. And that’s just the beginning! An automated alert system will inform 
+      a user when a rent is overdue or when real estate taxes need to be paid and so much more!
+      </p>
+      <p className="dark:text-gray-300">
+      REI Copilot gives real estate investors an assurance not possible with a spread sheet and with 
+      much less effort. With REI Copilot, a real estate investor will have the confidence that comes 
+      with KNOWING their numbers and the FREEDOM every investor seeks through truly passive income!
+      </p>
+    </div>
+    <div className="mt-8">
+      <div className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:!text-white sm:text-4xl">
+          <span className="block">Ready to take control?</span>
+          <span className="block text-primary-500">Start your free trial today.</span>
+        </h2>
+        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+          <div className="inline-flex rounded-md shadow">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-white
+              text-base font-medium rounded-md text-white bg-primary-500 hover:bg-primary-600 !no-underline"
+            >
+              Get started
+            </a>
+          </div>
+          <div className="ml-3 inline-flex rounded-md shadow">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent 
+                text-base font-medium rounded-md !text-primary-600 bg-white hover:bg-primary-50 !no-underline"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </React.Fragment>
 );
 
 export default OurStory;
